74a14d7ca8aefa741c794ae5f20b45fc15871ec4
Veranika <smeyun@list.ru>
2024-06-21 03:35:34 +0300

    init: start project

---
+++ frontend/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import { App } from 'app/App';
+import { store } from 'store/store.ts';
+
+import './index.css';
+
+import theme from './theme/theme';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
