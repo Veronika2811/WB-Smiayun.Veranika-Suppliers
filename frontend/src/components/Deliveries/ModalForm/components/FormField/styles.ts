23433289df202efd0ea2615148e6dbe2c840384f
Veranika <smeyun@list.ru>
2024-06-21 11:48:53 +0300

    feat: add Form field

---
+++ frontend/src/components/Deliveries/ModalForm/components/FormField/styles.ts
@@ -0,0 +1,12 @@
+export const formFieldSx = {
+  label: {
+    fontWeight: 900,
+    fontSize: '13px',
+    lineHeight: '123%',
+    letterSpacing: '0.01em',
+    fontFamily: 'ALSHauss, Arial, sans-serif',
+    paddingBottom: '8px',
+    color: 'rgba(255, 255, 255, 0.24)',
+    whiteSpace: 'pre-wrap',
+  },
+};
