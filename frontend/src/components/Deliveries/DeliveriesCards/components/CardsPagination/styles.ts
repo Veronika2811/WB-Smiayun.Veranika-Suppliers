544ebb8704c0c1e4fd6feccb5829b56d2c654900
Veranika <smeyun@list.ru>
2024-06-21 11:36:50 +0300

    feat: add pagination for tablet view

---
+++ frontend/src/components/Deliveries/DeliveriesCards/components/CardsPagination/styles.ts
@@ -0,0 +1,11 @@
+export const cardsPaginationSx = {
+  pagination: {
+    marginTop: 2,
+    '& .MuiPagination-ul': {
+      justifyContent: 'center',
+    },
+    '& .MuiButtonBase-root': {
+      color: '#ffffff',
+    },
+  },
+};
