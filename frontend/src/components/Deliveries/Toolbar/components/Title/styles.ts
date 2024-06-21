ac2a63ee178f964cfd0d2e8b7fe341a8928bbc98
Veranika <smeyun@list.ru>
2024-06-21 11:42:26 +0300

    feat: add title in toolbar

---
+++ frontend/src/components/Deliveries/Toolbar/components/Title/styles.ts
@@ -0,0 +1,10 @@
+export const titleSx = {
+  title: {
+    paddingTop: '5px',
+    fontFamily: 'ALSHauss, Helvetica, sans-serif',
+    fontSize: '32px',
+    fontWeight: '680',
+    lineHeight: '125%',
+    letterSpacing: '0.7px',
+  },
+};
